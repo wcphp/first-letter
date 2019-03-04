033479f446b0f998ca162361f09cbdaa4e1d06e9
weika <ka.wei@diandian7.com>
2019-03-04 20:05:29 +0800

    '去掉数字'

--- src/StringLetter.php
+++ src/StringLetter.php
@@ -12,7 +12,7 @@ class StringLetter
 {
     use WcToolsBase;
     /**
-     * 获取字符串首字符，字母（大小写）、数字直接返回，中文返回拼音的首字母，其它符号返回空
+     * 获取字符串首字符，字母（大小写）接返回，中文返回拼音的首字母，其它符号返回空
      * @access public
      * @param  string   $str 字符串
      * @param  bool     $type 返回字母大小写 true-大写，false-小写
@@ -24,7 +24,7 @@ class StringLetter
         if(empty($str)) return '';
         $fistStr = mb_substr( $str, 0, 1);
         $resStr = '';
-        if(preg_match('/[A-Za-z0-9]/',$fistStr)){
+        if(preg_match('/[A-Za-z]/',$fistStr)){
             $resStr = $fistStr;
         }elseif(preg_match('/[\x{4e00}-\x{9fff}]+/u',$fistStr)){
             $pinYinArr = json_decode(file_get_contents(__DIR__ . '/../data/pinyin-first.txt'),true);
